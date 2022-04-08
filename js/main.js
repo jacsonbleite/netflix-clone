1e084a1c7ad0b78816ad3611ba835aeecb141376
Jacson <jacson@box.localdomain>
2022-04-08 16:26:54 -0400

    Correção de bug no 'carosel' de trailers

--- js/main.js
+++ js/main.js
@@ -6,6 +6,48 @@ let iframeTrailer = document.createElement('iframe');
 let footerBottom = document.querySelector('.footer-bottom');
 let back = document.querySelector('.back');
 let trailerDestaque = document.querySelector('.trailer-destaque');
+let carroselFilmes = document.querySelector('.carrosel-filmes');
+
+
+
+let shortClick = function (cb) {
+    let min = 0;
+    let max = 50;
+    let time, self, timeout, event;
+
+    function reset() {
+        clearTimeout(timeout);
+        timeout = 0;
+    }
+
+    window.addEventListener('mouseup', reset); // caso o mouseup ocorra fora do elemento
+    return function (e) {
+        
+        if (!self) {
+            self = this;
+            self.addEventListener('mouseup', function (e) {                
+                let interval = new Date().getTime() - time;
+                if (timeout && interval > min) cb.call(self, event);
+                reset();
+            });
+        }
+        
+        event = e;
+        time = new Date().getTime();
+        
+        if (e.type == 'mousedown') timeout = setTimeout(reset, max);
+        
+    };
+};
+
+
+let handler = shortClick( (e) => {        
+    
+    boxesFilmes(boxFilme);        
+
+});
+
+carroselFilmes.addEventListener('mousedown', handler);
 
 
 
@@ -52,12 +94,16 @@ const createTrailer = (dataId) => {
 
 }
 
-boxFilme.forEach((value, index, array) => {
+const boxesFilmes = (boxFilme) => { 
+        
+    boxFilme.forEach((value, index, array) => {
+    
+        let item = array[index];        
+        trailerClicked(item);
 
-    let clicked = array[index];
-    trailerClicked(clicked);
+    })
 
-})
+}
 
 trailerDestaque.addEventListener('click', () => {
     
